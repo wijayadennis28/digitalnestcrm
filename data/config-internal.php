2902d30a1162250a215c7fbacba81bd493ca82c6
Dennis Wijaya <dennis.wijaya@thesmartlocal.com>
2024-07-09 10:06:58 +0700

    feat: crm 1st commit

---
+++ data/config-internal.php
@@ -0,0 +1,31 @@
+<?php
+return [
+  'database' => [
+    'host' => 'localhost',
+    'port' => NULL,
+    'charset' => NULL,
+    'dbname' => '',
+    'user' => '',
+    'password' => ''
+  ],
+  'smtpPassword' => NULL,
+  'logger' => [
+    'path' => 'data/logs/espo.log',
+    'level' => 'WARNING',
+    'rotation' => true,
+    'maxFileNumber' => 30,
+    'printTrace' => false
+  ],
+  'restrictedMode' => false,
+  'cleanupAppLog' => true,
+  'cleanupAppLogPeriod' => '30 days',
+  'webSocketMessager' => 'ZeroMQ',
+  'clientSecurityHeadersDisabled' => false,
+  'clientCspDisabled' => false,
+  'clientCspScriptSourceList' => [
+    0 => 'https://maps.googleapis.com'
+  ],
+  'adminUpgradeDisabled' => false,
+  'isInstalled' => false,
+  'microtimeInternal' => 1720494189.281321
+];
